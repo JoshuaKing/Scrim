d4c3aac051eb79acb747f3fc7d12c6e145607c0e
xzion <coen.mcclelland@gmail.com>
2014-07-19 07:25:14 +1000

    Finished team homepages

--- routes/team.js
+++ routes/team.js
@@ -4,29 +4,60 @@ var router = express.Router();
 
 /* Base team page */ 
 router.get('/', function(req, res) {
-	var db = req.db;
-	db.get("foo", function(err, value) {
-		if (err) throw(err);
-		console.log(value);
-		res.render('team', {
-			"title" : "Team page",
-			"foo" : value
-		});
-	});
+	res.redirect('/');
 });
 
 /* Base unique team page */ 
 router.get('/:id', function(req, res) {
 	var db = req.db;
 
-	db.get("foo", function(err, value) {
-		if (err) throw(err);
-		console.log(value);
+	db.hget("teamurls", req.params.id, function(err, reply) {
+		//console.log(reply);
 		
-		res.render('team', {
-			"title" : "Team page",
-			"foo" : req.params.id
-		});
+		if (reply) {
+			// Team Exists
+			res.locals.team = {};
+			res.locals.team.id = req.params.id;
+
+			var infodone, membersdone;
+
+			db.hgetall("team:"+reply, function(err, reply) {
+				
+				res.locals.team.info = reply;
+				// res.locals.team = {
+				// 	"info" : reply
+				// };
+				infodone = true;
+
+				if (infodone && membersdone) {
+					res.render('team', {
+						session : req.session
+					});
+				}
+
+				
+			});
+
+			db.smembers("team:"+reply+":players", function(err, reply) {
+				res.locals.team.players = reply;
+
+				membersdone = true;
+
+				if (infodone && membersdone) {
+					res.render('team', {
+						session : req.session
+					});
+				}
+			});
+
+			// Get schedule
+
+
+
+		} else {
+			// NO TEAM, HOME/404
+			res.redirect('/');
+		}
 	});
 });
 
